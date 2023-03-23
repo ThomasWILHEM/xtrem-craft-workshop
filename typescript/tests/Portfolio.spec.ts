6601f1bee0ff763671e7cf81e6217113532e6b3f
John THOMAS <john.thomas@etu.u-bordeaux.fr>
2023-03-23 09:11:51 +0100

    tests complex conversion

--- typescript/tests/Portfolio.spec.ts
+++ typescript/tests/Portfolio.spec.ts
@@ -8,7 +8,7 @@ class Portfolio {
   }
 
   evaluate (to: Currency, bank: Bank): number {
-    return this.count.reduce((acc: number,cur: {amount: number, currency: Currency}): number => {
+    return this.count.reduce((acc: number, cur: {amount: number, currency: Currency}): number => {
       return acc + bank.convert(cur.amount, cur.currency, to)
     }, 0)
   }
@@ -44,17 +44,30 @@ describe('Portfolio', function () {
     expect(result).toBe(2200)
   })
 
+  test('5 USD + 10 EUR = 18940 KRW', () => {
+    // Arrange
+    const bank = Bank.withExchangeRate(Currency.USD, Currency.KRW, 1100)
+    bank.addExchangeRate(Currency.EUR, Currency.KRW, 1344)
+    const portfolio = new Portfolio()
+    portfolio.add(5, Currency.USD)
+    portfolio.add(10, Currency.EUR)
+
+    // Act
+    const result = portfolio.evaluate(Currency.KRW, bank)
+
+    // Assert
+    expect(result).toBe(18940)
+  })
+
   it('should be evaluated to 0 when empty', () => {
     const portfolio = new Portfolio()
 
     const result = portfolio.evaluate(Currency.USD, bank)
 
     expect(result).toBe(0)
-
   })
 
   it('', () => {
-
     // Arange
     const portfolio = new Portfolio()
     portfolio.add(5, Currency.USD)
@@ -64,6 +77,5 @@ describe('Portfolio', function () {
 
     // Assert
     expect(result).toBe(5)
-
   })
 })
