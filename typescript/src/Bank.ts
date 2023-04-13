4d1d312e357a36b18ad4e25d27c312e2697c2faf
John THOMAS <john.thomas@etu.u-bordeaux.fr>
2023-04-13 17:36:43 +0200

    multicplier null ( = 0 ) check

--- typescript/src/Bank.ts
+++ typescript/src/Bank.ts
@@ -1,6 +1,7 @@
 import { Currency } from './Currency'
 import { MissingExchangeRateError } from './MissingExchangeRateError'
 import { Money } from './Money'
+import { ImpossibleMultiplierException } from './ImpossibleMultiplierException'
 
 export class Bank {
   private readonly _exchangeRates: Map<string, number> = new Map()
@@ -18,6 +19,11 @@ export class Bank {
   convert (money: Money, to: Currency) {
     if (!(money.currency === to || this._exchangeRates.has(money.currency + '->' + to))) {
       throw new MissingExchangeRateError(money.currency, to)
+    }
+
+    const multiplier: number = this._exchangeRates.get(money.currency + '->' + to)
+    if (multiplier <= 0.0) {
+      throw new ImpossibleMultiplierException(multiplier)
     }
 
     return money.currency === to
